6fd395faa15feaf7529d37377ecc86d545a9f34e
SaraBasaran <sarabasaran195@gmail.com>
2023-09-28 21:00:13 +0300

    09/28/2023

---
+++ src/main/java/practice_dt/Practice02_InterviewQuestion.java
@@ -0,0 +1,35 @@
+package practice_dt;
+
+public class Practice02_InterviewQuestion {
+    public static void main(String[] args) {
+
+        //Example1: Type code to swap two integers
+        //          a= 12 and b=5  ==> a=5 and b= 12
+        //1.way: by using a third empty container
+        int a=12;
+        int b= 5;
+        System.out.println(a+b);
+        System.out.println("Before swapping: "+ a+ " "+ b);
+
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping: " + a + " " + b);
+
+        //2.way: do not create a third container
+
+        int x= 12;
+        int y=5;
+        System.out.println("before swapping: " + x+ " "+ y);
+        x=x+y;
+
+        y= x-y;
+
+        x= x-y;
+
+        System.out.println("after swapping: "+ x+ " " + y);
+
+    }
+
+}
---
+++ src/main/java/practice_dt/Practice06_ScannerClass_GettingInitials.java
@@ -0,0 +1,32 @@
+package practice_dt;
+
+import java.util.Scanner;
+
+public class Practice06_ScannerClass_GettingInitials {
+    public static void main(String[] args) {
+
+        //Example: Ask user to enter his name and get the initial of a name which contains first name and last name
+        //         Tom Hanks ==> TH
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Enter your firstname and last name...");
+
+        String fullname= input.nextLine();
+
+
+
+       char firstChar= fullname.charAt(0); // T
+
+       char lastnameFirstChar= fullname.split(" ")[1].charAt(0);
+
+        System.out.println(firstChar+ " " + lastnameFirstChar);
+
+
+
+
+
+    }
+
+
+}
